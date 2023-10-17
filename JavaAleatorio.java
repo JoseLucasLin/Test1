0f7c10e5e6052bbc072499072b5568b84138ea5b
Lucas <o@o>
2023-10-17 20:24:50 +0200

    añadido un contador de letras strings

--- JavaAleatorio.java
+++ JavaAleatorio.java
@@ -1,6 +1,13 @@
-public class JavaAleatorio {
- public static void main (String[arg ]){
-System.out.println("aqui va el mensaje que quiero transmitir");
+import java.util.Scanner;
 
+public class JavaAleatorio{
+public static void main(String[] args) {
+	String out;
+System.out.println("aqui va el mensaje que quiero transmitir");
+Scanner input = new Scanner(System.in);
+out = input.nextLine();
+int numero = out.length();
+System.out.println(numero);
 	}
 }
+
